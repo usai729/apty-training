5c8bdd700e5551ee704b9ef6b5cb708594d36c52
usa729 <usai7261@gmail.com>
2024-12-02 21:58:33 +0530

    updated text

--- javascript/2-vars-data-types/u-sainath-rao/scripts/assignment-3.js
+++ javascript/2-vars-data-types/u-sainath-rao/scripts/assignment-3.js
@@ -8,22 +8,22 @@ const objectExample = { name: "Sai", age: 21 };
 
 document.getElementById(
 	"string-type"
-).innerHTML = `Data type of stringExample: ${typeof stringExample}`;
+).innerHTML = `stringExample: ${typeof stringExample}`;
 document.getElementById(
 	"number-type"
-).innerHTML = `Data type of numberExample: ${typeof numberExample}`;
+).innerHTML = `numberExample: ${typeof numberExample}`;
 document.getElementById(
 	"boolean-type"
-).innerHTML = `Data type of booleanExample: ${typeof booleanExample}`;
+).innerHTML = `booleanExample: ${typeof booleanExample}`;
 document.getElementById(
 	"undefined-type"
-).innerHTML = `Data type of undefinedExample: ${typeof undefinedExample}`;
+).innerHTML = `undefinedExample: ${typeof undefinedExample}`;
 document.getElementById(
 	"null-type"
-).innerHTML = `Data type of nullExample: ${typeof nullExample}`;
+).innerHTML = `nullExample: ${typeof nullExample}`;
 document.getElementById(
 	"symbol-type"
-).innerHTML = `Data type of symbolExample: ${typeof symbolExample}`;
+).innerHTML = `symbolExample: ${typeof symbolExample}`;
 document.getElementById(
 	"object-type"
-).innerHTML = `Data type of objectExample: ${typeof objectExample}`;
+).innerHTML = `objectExample: ${typeof objectExample}`;
