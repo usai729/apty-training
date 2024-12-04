2c6a61e1177e405e2a88f37b0b926b3b574f09fc
usa729 <usai7261@gmail.com>
2024-12-04 18:46:57 +0530

    added an array and a map

--- javascript/2-vars-data-types/u-sainath-rao/scripts/assignment-5.js
+++ javascript/2-vars-data-types/u-sainath-rao/scripts/assignment-5.js
@@ -1,11 +1,22 @@
-console.log("'5' + '10' = " + ("5" + "10"));
-console.log("5 + '10' = " + (5 + "10"));
-console.log("5 + 5 + '10' = " + (5 + 5 + "10"));
-console.log("'5' + 10 = " + ("5" + 10));
-console.log("true + 10 = " + (true + 10));
-console.log("false + 10 = " + (false + 10));
-console.log("5 + null = " + (5 + null));
-console.log("5 + undefined = " + (5 + undefined));
-console.log("'5' + null = " + ("5" + null));
-console.log("{} + '10' = " + ({} + "10"));
-console.log("Symbol('A') + '10' = Error! (Can't add a Symbol to a string)");
+const demo = document.getElementById("demo");
+
+const statements = [
+	"'5' + '10' = " + ("5" + "10"),
+	"5 + '10' = " + (5 + "10"),
+	"5 + 5 + '10' = " + (5 + 5 + "10"),
+	"'5' + 10 = " + ("5" + 10),
+	"true + 10 = " + (true + 10),
+	"false + 10 = " + (false + 10),
+	"5 + null = " + (5 + null),
+	"5 + undefined = " + (5 + undefined),
+	"'5' + null = " + ("5" + null),
+	"{} + '10' = " + ({} + "10"),
+	"Symbol('A') + '10' = Error! (Can't add a Symbol to a string)",
+];
+
+statements.map((statement) => {
+    let temp_ele = document.createElement("p");
+    temp_ele.innerHTML = `${statement}`;
+
+    demo.appendChild(temp_ele);
+});
